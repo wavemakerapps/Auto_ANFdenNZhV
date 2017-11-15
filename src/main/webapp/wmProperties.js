79e5d9638c16270d91f044dd22a259c416f05962
apptest <appTest1@wavemaker.com>
2017-11-15 20:02:34 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ANFdenNZhV",
+  "homePage" : "Main",
+  "name" : "Auto_ANFdenNZhV",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
